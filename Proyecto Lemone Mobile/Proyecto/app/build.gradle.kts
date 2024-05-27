7135e134063ba4f1b1912454138b2238a42f7adc
melisaapaz <melisaapaz@gmail.com>
2024-05-27 19:29:55 -0300

    Poder exportar el informe de ordenes - V1

--- Proyecto Lemone Mobile/Proyecto/app/build.gradle.kts
+++ Proyecto Lemone Mobile/Proyecto/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ispc.lemone"
-        minSdk = 24
+        minSdk = 26  // Cambiar de 24 a 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -42,7 +42,9 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("net.sourceforge.jexcelapi:jxl:2.6.12")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("net.sourceforge.jexcelapi:jxl:2.6.12")
 }
