9282dac665b8ed656c00955265815f3516e5aa42
Fabiola Benitez <fabiolaben03@gmail.com>
2024-06-02 16:37:27 -0300

    AcercaDe con boton de descarga de Lemone - fbenitez

--- Proyecto Lemone Mobile/Proyecto/app/src/main/java/com/ispc/lemone/activities/AcercaDe.java
+++ Proyecto Lemone Mobile/Proyecto/app/src/main/java/com/ispc/lemone/activities/AcercaDe.java
@@ -6,29 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.net.Uri;
 import com.ispc.lemone.R;
 
+
 public class AcercaDe extends AppCompatActivity {
-
-    private Button menuPricipal;
-
+    private Button btnMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
 
-
-        menuPricipal = findViewById(R.id.btnMenu);
-
-        menuPricipal.setOnClickListener(new View.OnClickListener() {
+        btnMenu = findViewById(R.id.btnMenu);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AcercaDe.this, Login.class);
+            public void onClick(View v) {
+                String url = "https://easy1089.github.io/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                 startActivity(intent);
             }
         });
-
     }
 }
-
